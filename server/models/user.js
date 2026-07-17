e2eb8eaba1d207803c5dd6a160ba47e0af86deb0
Dylan Pee <peedylan94@gmail.com>
2026-07-17 12:19:26 +0930

    Adding more notes for future changes in regards to video and user models

--- server/models/user.js
+++ server/models/user.js
@@ -1,6 +1,8 @@
 import mongoose from "mongoose";
 const { Schema } = mongoose;
 
+// Todo: Think of what other fields to have
+
 const userSchema = new Schema({
   firstName: String,
   lastName: String,
--- server/models/video.js
+++ server/models/video.js
@@ -3,6 +3,8 @@ import userSchema from "./user.js";
 const { Schema } = mongoose;
 const User = mongoose.model("User", userSchema);
 
+// Todo: Think of what other fields to have, upload timestamp, upload author, duration
+
 const videoSchema = new Schema({
   user: { type: mongoose.ObjectId, ref: User },
   url: String,

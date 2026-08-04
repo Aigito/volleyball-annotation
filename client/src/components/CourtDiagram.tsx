d5a2ee97bde965e59de30d36bd96494f008ff5b4
Dylan Pee <peedylan94@gmail.com>
2026-08-04 17:13:18 +0930

    Test canvas by drawing a circle on it

--- client/src/components/CourtDiagram.tsx
+++ client/src/components/CourtDiagram.tsx
@@ -1,5 +1,14 @@
+import { useEffect, useRef } from "react";
+
 export default function CourtDiagram() {
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
+
+  useEffect(() => {
+    const ctx = canvasRef?.current?.getContext("2d");
+    ctx.beginPath();
+    ctx.arc(95, 50, 40, 0, 2 * Math.PI);
+    ctx.stroke();
+  }, []);
 
   return (
     <canvas

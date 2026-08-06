ee613bcdeae8a55c276573068ef2c55d511fb949
Dylan Pee <peedylan94@gmail.com>
2026-08-06 15:06:57 +0930

    Simulating drawing a line by logging mouse coordinates

--- client/src/components/CourtDiagram.tsx
+++ client/src/components/CourtDiagram.tsx
@@ -4,6 +4,7 @@ import drawStaticCourt from "../canvasHelpers/drawStaticCourt";
 export default function CourtDiagram() {
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
   const nextPointRef = useRef<{ x: number; y: number } | null>(null);
+  const drawModeRef = useRef(false);
 
   useEffect(() => {
     const ctx = canvasRef.current?.getContext("2d");
@@ -29,21 +30,33 @@ export default function CourtDiagram() {
       };
     };
 
-    const grabNextPoint = (e: MouseEvent) => {
-      nextPointRef.current = getCanvasPoint(e);
+    const handleMouseMove = (e: MouseEvent) => {
+      if (drawModeRef.current) {
+        nextPointRef.current = getCanvasPoint(e);
+
+        // TODO: Change this to actually drawing a line
+        console.log(nextPointRef.current);
+      }
     };
 
-    const logNextPoint = (e: MouseEvent) => {
-      nextPointRef.current = getCanvasPoint(e);
-      console.log("next point", nextPointRef.current);
+    const handleMouseDown = () => {
+      drawModeRef.current = true;
     };
 
-    canvas.addEventListener("mousemove", grabNextPoint);
-    canvas.addEventListener("mousedown", logNextPoint);
+    const handleMouseUp = () => {
+      drawModeRef.current = false;
+    };
+
+    // TODO: To implement setting draw mode to false when mouse leaves canvas
+
+    canvas.addEventListener("mousedown", handleMouseDown);
+    canvas.addEventListener("mouseup", handleMouseUp);
+    canvas.addEventListener("mousemove", handleMouseMove);
 
     return () => {
-      canvas.removeEventListener("mousemove", grabNextPoint);
-      canvas.removeEventListener("mousedown", logNextPoint);
+      canvas.removeEventListener("mousemove", handleMouseMove);
+      canvas.removeEventListener("mousedown", handleMouseDown);
+      canvas.removeEventListener("mouseup", handleMouseUp);
     };
   }, []);
 

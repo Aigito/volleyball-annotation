69f4473d043b63496c3311e3aab354590e349e50
Dylan Pee <peedylan94@gmail.com>
2026-08-25 17:15:33 +0930

    Added videos view page and reimplement routing

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,5 +1,12 @@
+import { Outlet } from "react-router";
+
 function Home() {
-  return "Home Page";
+  return (
+    <>
+      "Home Page" "Nav bar goes here"
+      <Outlet />
+    </>
+  );
 }
 
 export default Home;
--- client/src/router.tsx
+++ client/src/router.tsx
@@ -1,10 +1,26 @@
-import { createBrowserRouter } from "react-router";
+import { createBrowserRouter, Navigate } from "react-router";
 import Home from "./pages/Home";
 import Video from "./pages/Video";
 import Settings from "./pages/Settings";
+import VideosList from "./pages/VideosList";
 
 export const router = createBrowserRouter([
-  { path: "/", element: <Home /> },
-  { path: "/videos", element: <Video /> },
-  { path: "/settings", element: <Settings /> },
+  {
+    path: "/",
+    element: <Home />,
+    children: [
+      { index: true, element: <Navigate to="/videos" /> },
+      {
+        path: "videos",
+        children: [
+          { index: true, element: <VideosList /> },
+          {
+            path: ":videoId",
+            element: <Video />,
+          },
+        ],
+      },
+      { path: "settings", element: <Settings /> },
+    ],
+  },
 ]);
